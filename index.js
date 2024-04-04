3a8750bfdc90e4b226b887e14fc563320f8db51e
Ralph Reinheimer <rrein@users.noreply.github.com>
2024-04-04 19:15:39 +0000

    updated index.js file

---
+++ index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+const MarkdownEditor = () => {
+    const [markdown, setMarkdown] = useState('type markdown here');
+
+    const handleMarkdownChange = (event) => {
+        setMarkdown(event.target.value);
+    };
+
+    return (
+        <div>
+            <textarea value={markdown} onChange={handleMarkdownChange} />
+
+            <div>
+                <ReactMarkdown>{markdown}</ReactMarkdown>
+            </div>
+        </div>
+    );
+};
+
+export default MarkdownEditor;
+
+const sentence = "Hello, world!";
+const reversedSentence = sentence.split('').reverse().join('');
+const capitalizedSentence = reversedSentence.charAt(0).toUpperCase() + reversedSentence.slice(1);
+console.log(capitalizedSentence);
+
+const data = [
+    [{ name: 'John' }, { name: 'Jane' }],
+    [{ name: 'Bob' }]
+];
+
+const names = data.flatMap(arr => arr.map(obj => obj.name));
+console.log(names);
